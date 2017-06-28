de04540bfb63a7d44dc31b9ece7319fd21588188
Rommel Mᴀʀᴛɪɴᴇᴢ <ebzzry@ebzzry.io>
2017-06-28 22:23:11 +0800

    Add unix.lisp

---
+++ unix.lisp
@@ -0,0 +1,89 @@
+(uiop:define-package
+    :cl-scripts/unix
+    (:use
+     :cl
+     :fare-utils
+     :uiop
+     :inferior-shell
+     :cl-scripting
+     :optima
+     :optima.ppcre
+     :cl-ppcre
+     :local-time
+     :cl-launch/dispatch
+     :cl-scripts/misc
+     :cl-scripts/utils)
+  (:export #:x
+           #:c
+           #:v
+           #:f
+           #:md
+           #:sl
+
+           #:l0
+           #:la
+           #:lk
+           #:l
+           #:ll
+           #:lr
+           #:lh
+           #:l1
+           #:lv
+
+           #:g
+           #:gi))
+
+(in-package :cl-scripts/unix)
+
+(exporting-definitions
+ (defun x (&rest args)
+   (run/i `(zsh -c ,@args)))
+ (defun c (&rest args)
+   (run/i `(cat ,@args))
+   (success))
+ (defun v (&rest args)
+   (run/i `(less ,@args))
+   (success))
+ (defun f (arg)
+   (run/i `(find "." -iname ,(format nil "*~A*" arg)))
+   (success))
+ (defun md (&rest args)
+   (run/i `(mkdir -p ,@args))
+   (success))
+ (defun rm! (&rest args)
+   (run/i `(rm -rf ,@args))
+   (success))
+ (defun sl (&rest args)
+   (run/i `(ln -sf ,@args))
+   (success))
+
+ (defun l0 (&rest args)
+   (run/i `(ls "-A" "-F" "--color" ,@args))
+   (success))
+ (defun la (&rest args)
+   (apply-args-1 'l0 args :options '("-A" "-F" "--color")))
+ (defun lk (&rest args)
+   (apply-args-1 'la args :options '("-l")))
+ (defun l (&rest args)
+   (apply-args-1 'la args :options '("-tr")))
+ (defun ll (&rest args)
+   (apply-args-1 'l args :options '("-l")))
+ (defun lr (&rest args)
+   (apply-args-1 'l args :options '("-R")))
+ (defun lh (&rest args)
+   (apply-args-1 'l args :options '("-H")))
+ (defun l1 (&rest args)
+   (apply-args-1 'l args :options '("-1")))
+ (defun lv (&rest args)
+   (let ((arguments (if (null args) '("*") args)))
+     (run/i `(pipe (l ,@args)
+                   (less)))
+     (success)))
+
+ (defun g (&rest args)
+   (run/i `(egrep "--color" ,@args))
+   (success))
+ (defun gi (&rest args)
+   (apply-args-1 'g args :options '("-i"))))
+
+(register-commands :cl-scripts/unix)
