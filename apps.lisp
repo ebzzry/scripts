a4ea611d3a8fe7e51aad798cc293fc5f030a0753
Rommel Mᴀʀᴛɪɴᴇᴢ <ebzzry@ebzzry.io>
2017-08-04 13:33:25 +0800

    Rename lisp -> lisp0

--- apps.lisp
+++ apps.lisp
@@ -1,34 +1,37 @@
 (uiop:define-package
     :scripts/apps
-    (:use
-     :cl
-     :fare-utils
-     :uiop
-     :inferior-shell
-     :cl-scripting
-     :optima
-     :optima.ppcre
-     :cl-ppcre
-     :local-time
-     :cl-launch/dispatch
-     :scripts/misc
-     :scripts/utils)
-  (:export #:xrsync
-           #:ra
-           #:raz
+    (:use :cl
+          :fare-utils
+          :uiop
+          :inferior-shell
+          :cl-scripting
+          :optima
+          :optima.ppcre
+          :cl-ppcre
+          :local-time
+          :cl-launch/dispatch
+          :scripts/misc
+          :scripts/utils)
+    (:export #:xrsync
+             #:ra
+             #:raz
 
-           #:chrome
-           #:chrome-stable
-           #:kill-chrome
-           #:stop-chrome
-           #:continue-chrome
+             #:chrome
+             #:chrome-stable
+             #:kill-chrome
+             #:stop-chrome
+             #:continue-chrome
 
-           #:suma
-           #:askpass
-           #:lisp
-           #:screenshot
-           #:sg2e
-           #:sg2eb))
+             #:suma
+             #:term
+
+             #:kill-apps
+
+             #:askpass
+             #:lisp0
+             #:screenshot
+             #:sg2e
+             #:sg2eb))
 
 (in-package :scripts/apps)
 
@@ -66,11 +69,19 @@
    (run/nil `(wine ,(home ".wine/drive_c/Program Files/SumatraPDF/SumatraPDF.exe") ,@args) :on-error nil)
    (success))
 
+ (defun term (&rest args)
+   (run/nil `(terminator ,@args) :on-error nil)
+   (success))
+
+ (defun kill-apps ()
+   (loop :for app :in '("chrome" "viber" "telegram")
+      :do (run `(killall "-9" ,app))))
+
  (defun askpass ()
    (run/i `(git gui--askpass))
    (values))
 
- (defun lisp (&rest args)
+ (defun lisp0 (&rest args)
    (let* ((arguments (mapcar #'(lambda (s) (format nil "\'~A\'" s)) args))
           (list-arguments (append '("sbcl") arguments))
           (string-arguments (format nil "~{~a~^ ~}" list-arguments))
@@ -87,9 +98,9 @@
      (flet ((scrot (file dest &rest args)
               (run/i `(scrot ,@args ,file -e ,dest))))
        (match mode
-         ((ppcre "(full|plena)") (scrot file dest))
-         ((ppcre "(region|parta)") (scrot file dest '-s))
-         (_ (err (format nil "invalid mode ~A~%" mode))))
+              ((ppcre "(full|plena)") (scrot file dest))
+              ((ppcre "(region|parta)") (scrot file dest '-s))
+              (_ (err (format nil "invalid mode ~A~%" mode))))
        (run `(xclip -selection clipboard) :input (list image))
        (success))))
 
@@ -100,6 +111,5 @@
  (defun sg2eb (&rest args)
    (run/i `(stem "-X" ,(argv0) "steam://rungameid/208610"))
    (success)))
-
 
 (register-commands :scripts/apps)
