8ec7466c49f78f403003a7527888b37a8ed065df
Rommel MARTÍNEZ <rommel.martinez@astn-group.com>
2018-02-01 01:54:06 +0800

    Write RUN-PROFILE

--- default.nix
+++ default.nix
@@ -4,9 +4,6 @@ with pkgs;
 
 stdenv.mkDerivation {
   name = "lisp-shell";
-  buildInputs = [ bash coreutils sbcl rlwrap openssl git nix-repl ];
-
-  LD_LIBRARY_PATH = stdenv.lib.makeLibraryPath [
-    pkgs.openssl
-  ];
+  buildInputs = [ bash coreutils sbcl rlwrap openssl git nix-repl qt5.qtwebkit gnome3.webkitgtk ];
+  LD_LIBRARY_PATH = stdenv.lib.makeLibraryPath [ pkgs.openssl ];
 }
