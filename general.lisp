7036b92ecb280b4ffae0505fa02888e767053835
Rommel Mᴀʀᴛɪɴᴇᴢ <rommel@mind.ai>
2018-06-10 10:19:23 +0800

    Clean up general.lisp

--- general.lisp
+++ general.lisp
@@ -17,28 +17,51 @@
            #:*colon-mode*
            #:*normal-mode*
            #:*num-mode*
-
            #:ascii-hex-table
-           #:ascii
            #:ascii-oct-table
-
            #:rot13
            #:battery
            #:trackpoint
-           #:xdev-id
-           #:xdev
-           #:xmap
-
-           #:load-xmodmap
-           #:load-xresources
-
            #:xxx
-
            #:psg
            #:psk
            #:psk!))
 
 (in-package :scripts/general)
+
+(defun xdev-id (name type)
+  (format nil "~A"
+          (regex-replace
+           (create-scanner ".*id=(.*?)	+.*")
+           (first (remove-if (complement
+                              #'(lambda (line)
+                                  (and (search name line) (search (format nil "slave  ~A" type) line))))
+                             (run-program '("xinput" "list") :output :lines))) "\\1")))
+
+(defun xdev (name type command &rest args)
+  (let ((id (xdev-id name type)))
+    (when (not (string= id "NIL"))
+      (run/i `(xinput ,command ,(parse-integer id) ,@args))
+      (success))))
+
+(defun xmap (keymap)
+  (run/i `(setxkbmap dvorak))
+  (run/i `(xset r rate 250))
+  (run/i `(xmodmap ,(home (format nil "hejmo/ktp/xmodmap/.Xmodmap.~A" keymap))))
+  (success))
+
+(defun load-xmodmap (device)
+  (if (remove-if (complement #'(lambda (line) (search device line)))
+                 (run-program '("lsusb") :output :lines))
+      (xmap "advantage.dvorak")
+      (if (string-equal (hostname) "vulpo")
+          (xmap "thinkpad.dvorak")
+          (xmap "ceteraj.dvorak")))
+  (success))
+
+(defun load-xresources ()
+  (run `(xrdb ,(home ".Xresources")) :output :interactive :input :interactive :error-output nil :on-error nil)
+  (success))
 
 (exporting-definitions
   (defvar *num-mode* "[31m")
@@ -55,8 +78,6 @@
                       *normal-mode*
                       (zerop (mod (1+ i) 16))))
     (success))
-
-  (defun ascii () (ascii-hex-table))
 
   (defun ascii-oct-table ()
     (loop :for i :from 32 :to 255
@@ -83,40 +104,6 @@
       (run/i `(xinput set-prop ,device "Evdev Wheel Emulation Timeout" 200))
       (run/i `(xinput set-prop ,device "Evdev Wheel Emulation Axes" 7 6 5 4))
       (success)))
-
-  (defun xdev-id (name type)
-    (format nil "~A"
-            (regex-replace
-             (create-scanner ".*id=(.*?)	+.*")
-             (first (remove-if (complement
-                                #'(lambda (line)
-                                    (and (search name line) (search (format nil "slave  ~A" type) line))))
-                               (run-program '("xinput" "list") :output :lines))) "\\1")))
-
-  (defun xdev (name type command &rest args)
-    (let ((id (xdev-id name type)))
-      (when (not (string= id "NIL"))
-        (run/i `(xinput ,command ,(parse-integer id) ,@args))
-        (success))))
-
-  (defun xmap (keymap)
-    (run/i `(setxkbmap dvorak))
-    (run/i `(xset r rate 250))
-    (run/i `(xmodmap ,(home (format nil "hejmo/ktp/xmodmap/.Xmodmap.~A" keymap))))
-    (success))
-
-  (defun load-xmodmap (device)
-    (if (remove-if (complement #'(lambda (line) (search device line)))
-                   (run-program '("lsusb") :output :lines))
-        (xmap "advantage.dvorak")
-        (if (string-equal (hostname) "vulpo")
-            (xmap "thinkpad.dvorak")
-            (xmap "ceteraj.dvorak")))
-    (success))
-
-  (defun load-xresources ()
-    (run `(xrdb ,(home ".Xresources")) :output :interactive :input :interactive :error-output nil :on-error nil)
-    (success))
 
   (defun xxx ()
     (let ((hostname (hostname))
