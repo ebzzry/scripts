537a358e8eccad9f5e2d3116e4ce079a93cd54f4
Rommel MARTÍNEZ <rommel.martinez@astn-group.com>
2018-06-26 12:18:55 +0800

    Add proper webcam search

--- webcam.lisp
+++ webcam.lisp
@@ -9,7 +9,8 @@
           #:scripts/misc
           #:scripts/unix
           #:scripts/utils)
-  (:export #:disable-internal-webcam
+  (:export #:enable-integrated-webcam
+           #:disable-integrated-webcam
            #:set-zoom
            #:reset-zoom
            #:decrease-zoom
@@ -22,6 +23,10 @@
 (defparameter *device*
   "/dev/video0"
   "The default webcam device")
+
+(defparameter *directory-wildcard*
+  "/sys/bus/usb/devices/*/product"
+  "The pathname wildcard for the USB devices")
 
 (mof:defcon +increments+
   10
@@ -52,63 +57,92 @@
                           `("v4l2-ctl" "-d" ,device "-l"))))
    :separator '(#\space)))
 
-(defun zoom-value (type)
+(defun zoom-value (type &optional (device *device*))
   "Return the current absolute zoom value for TYPE."
   (values
    (parse-integer
     (cl-ppcre:regex-replace
      (mof:fmt "~A=(.*)" type)
-     (first (remove-if-not #'(lambda (text) (search type text)) (zoom-settings)))
+     (first (remove-if-not #'(lambda (text) (search type text)) (zoom-settings device)))
      "\\1"))))
 
-(defparameter *default-zoom* (zoom-value "default")
-  "Return the default zoom value.")
+(defun get-default-zoom (&optional (device *device*))
+  "Return the default zoom value."
+  (zoom-value "default" device))
 
-(defparameter *minimum-zoom* (zoom-value "min")
-  "Return the minimum zoom value for DEVICE.")
+(defun get-minimum-zoom (&optional (device *device*))
+  "Return the minimum zoom value for DEVICE."
+  (zoom-value "min" device))
 
-(defparameter *maximum-zoom* (zoom-value "max")
-  "Return the maximum zoom value for DEVICE.")
+(defun get-maximum-zoom (&optional (device *device*))
+  "Return the maximum zoom value for DEVICE."
+  (zoom-value "max" device))
+
+(defun search-device-id (name)
+  "Return the device ID of device with NAME."
+  (let* ((files (directory *directory-wildcard*))
+         (entry (first (remove-if-not #'(lambda (device)
+                                          (string-equal name (uiop:read-file-line device)))
+                                      files)))
+         (strings (remove-if #'mof:empty-string-p
+                             (split-sequence:split-sequence
+                              #\/
+                              (directory-namestring
+                               (uiop:native-namestring entry)))))
+         (id (mof:last* strings)))
+    id))
+
+(defun get-integrated-camera-id ()
+  "Return the ID of the integrated camera."
+  (search-device-id "Integrated Camera"))
 
 (exporting-definitions
-  (defun disable-internal-webcam ()
-    "Disable the internal webcam."
-    (sush "echo 2-8 > /sys/bus/usb/drivers/usb/bind"))
+ (defun enable-integrated-webcam ()
+   "Enable the integrated webcam."
+   (let* ((id (get-integrated-camera-id))
+          (fmt (mof:fmt "echo ~A > /sys/bus/usb/drivers/usb/bind" id)))
+     (sush fmt)))
 
-  (defun set-zoom (device value)
-    "Set a specific zoom value."
-    (run-command/i device "-c" (mof:fmt "zoom_absolute=~A" value))
-    (current-zoom))
+ (defun disable-integrated-webcam ()
+   "Disable the integrated webcam."
+   (let* ((id (get-integrated-camera-id))
+          (fmt (mof:fmt "echo ~A > /sys/bus/usb/drivers/usb/unbind" id)))
+     (sush fmt)))
 
-  (defun reset-zoom (&optional (device *device*))
-    "Set the zoom to the default vaule."
-    (set-zoom device *default-zoom*)
-    (current-zoom))
+ (defun set-zoom (device value)
+   "Set a specific zoom value."
+   (run-command/i device "-c" (mof:fmt "zoom_absolute=~A" value))
+   (current-zoom))
 
-  (defun decrease-zoom (&optional (device *device*))
-    "Decrease the zoom setting."
-    (let* ((current (current-zoom))
-           (new (- current +increments+))
-           (value (if (< new *minimum-zoom*)
-                      *minimum-zoom*
-                      new)))
-      (set-zoom device value)))
+ (defun reset-zoom (&optional (device *device*))
+   "Set the zoom to the default vaule."
+   (set-zoom device (get-default-zoom))
+   (current-zoom))
 
-  (defun increase-zoom (&optional (device *device*))
-    "Decrease the zoom setting."
-    (let* ((current (current-zoom))
-           (new (+ current +increments+))
-           (value (if (> new *maximum-zoom*)
-                      *maximum-zoom*
-                      new)))
-      (set-zoom device value)))
+ (defun decrease-zoom (&optional (device *device*))
+   "Decrease the zoom setting."
+   (let* ((current (current-zoom))
+          (new (- current +increments+))
+          (value (if (< new (get-minimum-zoom))
+                     (get-minimum-zoom)
+                     new)))
+     (set-zoom device value)))
 
-  (defun minimum-zoom (&optional (device *device*))
-    "Set the zoom to the lowest setting."
-    (set-zoom device *minimum-zoom*))
+ (defun increase-zoom (&optional (device *device*))
+   "Decrease the zoom setting."
+   (let* ((current (current-zoom))
+          (new (+ current +increments+))
+          (value (if (> new (get-maximum-zoom))
+                     (get-maximum-zoom)
+                     new)))
+     (set-zoom device value)))
 
-  (defun maximum-zoom (&optional (device *device*))
-    "Set the zoom to the highest setting."
-    (set-zoom device *maximum-zoom*)))
+ (defun minimum-zoom (&optional (device *device*))
+   "Set the zoom to the lowest setting."
+   (set-zoom device (get-minimum-zoom)))
+
+ (defun maximum-zoom (&optional (device *device*))
+   "Set the zoom to the highest setting."
+   (set-zoom device (get-maximum-zoom))))
 
 (register-commands :scripts/webcam)
