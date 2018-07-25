61cea3dc67aad59f8c1e8142218c48b05d40bc25
Rommel MARTÍNEZ <rommel.martinez@astn-group.com>
2018-07-25 14:05:09 +0800

    Clean up

--- utils.lisp
+++ utils.lisp
@@ -28,6 +28,7 @@
 (in-package #:scripts/utils)
 
 (defun char-display-char (c)
+  "Display character C to stdout."
   (if (or (member c '(127 155))
           (< c 32)
           (<= 128 c 159))
@@ -35,6 +36,7 @@
       (code-char c)))
 
 (defun battery-status ()
+  "Display battery status."
   (let ((base-dir "/sys/class/power_supply/*")
         (exclude-string "/AC/"))
     (with-output (s nil)
@@ -48,59 +50,68 @@
         :do (format s "~A: ~A% (~A)~%" battery capacity status)))))
 
 (defun wine (path &rest args)
+  "Run PATH to wine."
   (run/i `(wine ,path ,@args)))
 
 (defun err (message)
+  "Exit with MESSAGE."
   (die 1 (format t "Error: ~A~%" message)))
 
 (defun apply-args (function options args)
+  "Apply FUNCTION to ARGS."
   (apply function (append (list options) args)))
 
 (defun apply-args-1 (function args &key (options nil))
+  "Apply FUNCTION to ARGS."
   (apply function (append options args)))
 
 (defun string-first (string)
+  "Return the first string from STRING."
   (let* ((space (position #\  string :test #'equal)))
     (subseq string 0 space)))
 
 (defun find-binary (binary)
+  "Find BINARY in PATH."
   (run/ss `(readlink -f ,(run/ss `(which ,binary)))))
 
 (defun run-with-locale (locale &rest args)
-  "Run args with locale set to LOCALE"
+  "Run args with locale set to LOCALE."
   (setf (getenv "LANG") locale)
   (run/i `(,@(first args)))
   (success))
 
 (defun run-with-nix-system (binary &rest args)
-  "Run binary without user paths"
+  "Run binary without user paths."
   (setf (getenv "PATH") "/var/setuid-wrappers:/run/wrappers/bin:/run/current-system/sw/bin:/run/current-system/sw/sbin:/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin")
   (run/i `(,binary ,@args))
   (success))
 
-(defun run-with-nix-user (profile binary &rest args)
-  "Run binary under a separate profile"
-  (let ((bin (mof:home (mof:fmt ".baf/profiles/~A/bin" profile))))
-    (setf (getenv "PATH") (unix-namestring bin))
-    (run/i `(,binary ,@args))
-    (success)))
-
 (defun run-with-xdg (binary &rest args)
-  "Run binary under a custom XDG_DATA_DIRS path"
+  "Run binary under a custom XDG_DATA_DIRS path."
   (setf (getenv "XDG_DATA_DIRS")
         (uiop:native-namestring (mof:home ".local/share/mime")))
   (run/i `(,binary ,@args))
   (success))
 
 (defmacro % (name command)
+  "Define a normal command runner."
   `(defun ,name (&rest args)
      (run/i (append (split "\\s+" ,command) args))
      (success)))
 
-(defun with-qt (name args)
+(defun run-with-nix-user (profile binary args)
+  "Run binary under a separate profile."
+  (let ((bin (mof:home (mof:fmt ".baf/profiles/~A/bin" profile))))
+    (setf (getenv "PATH") (unix-namestring bin))
+    (run/i `(,binary ,@args))
+    (success)))
+
+(defun with-qt (command args)
+  "Run a program in the QT profile."
   (setf (getenv "QT_QPA_PLATFORMTHEME") "qt5ct")
-  (run-with-nix-user "qt" name args))
+  (run-with-nix-user "qt" command args))
 
 (defmacro $ (name command)
+  "Define a runner in the QT profile."
   `(defun ,name (&rest args)
      (with-qt ,command args)))
